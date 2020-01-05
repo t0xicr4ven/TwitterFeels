fd5c2f155c12130c4bdee39ef453d9bb7048480b
Leila Bremner <sclgamingdevs@gmail.com>
2020-01-06 08:53:43 +1100

    added swiftyJSON

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreML
+import SwifteriOS
 
 
 class ViewController: UIViewController {
@@ -15,15 +16,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
+    let sentimentClassifier = TweetSentimentClassifier()
+    
+    let swifter = Swifter(consumerKey: "key", consumerSecret: "key")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       let prediction = try! sentimentClassifier.prediction(text: "You are shit")
+        
+        print(prediction.label)
+  
+        swifter.searchTweet(using: "@Apple",lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+            //print(results)
+            //print(metadata)
+        }) { (error) in
+            print("there was was an error with the twitter api request \(error)")
+        }
+  
     }
-
+    
+    
     @IBAction func predictPressed(_ sender: Any) {
-    
-    
+        
+        
     }
     
 }
