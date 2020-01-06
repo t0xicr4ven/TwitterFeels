93d0888ad32d6f440477f678ac48ae991a27fc57
Leila Bremner <sclgamingdevs@gmail.com>
2020-01-06 12:30:49 +1100

    finished produt

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 This app uses machine learning to gauge the sentiment of things you search on twitter.
+
+You will need your own Twitter API keys to make this work.
--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -19,29 +19,89 @@ class ViewController: UIViewController {
     
     let sentimentClassifier = TweetSentimentClassifier()
     
-    let swifter = Swifter(consumerKey: "key", consumerSecret: "key")
+    let swifter = Swifter(consumerKey: "USE YOUR OWN KEY", consumerSecret: "USE YOUR OWN KEY")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       let prediction = try! sentimentClassifier.prediction(text: "You are shit")
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         
-        print(prediction.label)
-  
-        swifter.searchTweet(using: "@Apple",lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-            //print(results)
-            //print(metadata)
-        }) { (error) in
-            print("there was was an error with the twitter api request \(error)")
-        }
-  
+       
+        
+        view.addGestureRecognizer(tapGesture)
+    
     }
     
     
     @IBAction func predictPressed(_ sender: Any) {
         
-        
+        if let searchText = textField.text {
+            
+            swifter.searchTweet(using: searchText ,lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+                
+                
+                var tweets = [TweetSentimentClassifierInput]()
+                
+                for i in 0..<100 {
+                    //grab the full text then convert it into something the model can undetstant
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassifiaction = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassifiaction)
+                    }
+                    
+                }
+                do{
+                    //get model to predict the sentiment of the text and put the output into an array
+                    let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                    
+                    var sentimentScore = 0
+                    
+                    //loop through the results and score each sentiment results, and add/subtract to total score
+                    for prediction in predictions {
+                        let sentiment = prediction.label
+                        
+                        if sentiment == "Pos" {
+                            sentimentScore += 1
+                        }else if sentiment == "Neg" {
+                            sentimentScore -= 1
+                        }
+                        
+                    }
+                    
+                    if sentimentScore > 20 {
+                        self.sentimentLabel.text = "😍"
+                    }else if sentimentScore > 10 {
+                        self.sentimentLabel.text = "😀"
+                    }else if sentimentScore > 0 {
+                        self.sentimentLabel.text = "🙂"
+                    }else if sentimentScore > -10 {
+                        self.sentimentLabel.text = "😐"
+                    }else if sentimentScore > -20 {
+                        self.sentimentLabel.text = "😡"
+                    }else {
+                        self.sentimentLabel.text = "🤮"
+                    }
+                    
+                    print(sentimentScore)
+                }catch{
+                    print("There was an error with making a prediction::\(error)")
+                }
+                
+                
+            }) { (error) in
+                print("there was was an error with the twitter api request \(error)")
+            }
+            
+            
+        }
+  
     }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
     
 }
 
